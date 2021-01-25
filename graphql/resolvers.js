0d8d6a78af2baa763f5e8a7ccff058ccf9e5de33
daldalhada <ssen199545@gmail.com>
2021-01-26 00:16:06 +0900

    graphql init

---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query: {
+        name: () => "daldalhada"
+    }
+};
+
+export default resolvers
---
+++ index.js
@@ -0,0 +1,10 @@
+import { GraphQLServer } from 'graphql-yoga';
+//const { GraphQLServer } = require('graphql-yoga')
+import resolvers from './graphql/resolvers';
+
+const server = new GraphQLServer({
+    typeDefs: "graphql/schema.graphql",
+    resolvers
+});
+
+server.start(() => console.log("Graphql Server Running"));
