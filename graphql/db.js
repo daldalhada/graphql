53d21133a2e3b5475fe463cffde8306d2f47bc5a
daldalhada <ssen199545@gmail.com>
2021-01-28 01:40:45 +0900

    mutation create

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,49 @@
-export const people = [
+export const movies = [
     {
         id: "0",
-        name: "dal",
-        age: 27,
-        gender: "male",
+        name: "Avengers1",
+        score: 4.5
     },
     {
         id: "1",
-        name: "daldal",
-        age: 27,
-        gender: "male",
+        name: "Avengers2:Age_Of_Ultron",
+        score: 4.3
     },
     {
         id: "2",
-        name: "daldaldal",
-        age: 27,
-        gender: "male",
-    }
+        name: "Avengers3:Infinity_War",
+        score: 4.6
+    },
+    {
+        id: "3",
+        name: "Avengers4:End_Game",
+        score: 4.7
+    },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
+    const filterMovies = movies.filter(movie => movie.id === String(id));
+    return filterMovies[0];
 };
+
+export const deleteMovies = (id) => {
+    const clearMovies = movies.filter(movie => movie.id !== String(id));
+    if(movies.length > clearMovies.length) {
+        movie = clearMovies;
+        return true;
+    } else {
+        return false;
+    }
+}; 
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+}
